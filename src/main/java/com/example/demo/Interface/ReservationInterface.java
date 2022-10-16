72ded42065dbfec3b67322347eada843fffe478c
gabriela <gabriela.rojas.mt@usa.edu.co>
2022-10-16 16:22:23 -0600

    reto5

---
+++ src/main/java/com/example/demo/Interface/ReservationInterface.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.example.demo.Interface;
+
+import com.example.demo.Modelo.Reservation;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Date;
+import java.util.List;
+
+
+public interface ReservationInterface extends CrudRepository <Reservation, Integer> {
+    /* Reto 5*/
+    public List<Reservation> findAllByStatus (String status);
+
+    public List<Reservation> findAllByStartDateAfterAndStartDateBefore (Date datoUno, Date datoDos);
+
+    @Query("SELECT c.client, COUNT(c.client) from Reservation AS c group by c.client order by COUNT(c.client) DESC")
+    public List<Object[]> countTotalReservationByClient();
+}
